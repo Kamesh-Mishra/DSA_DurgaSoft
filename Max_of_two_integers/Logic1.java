0cfcc30162c9913d058cc6dc58da615b3363f1cd
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2025-05-05 19:46:24 +0530

    Added new file

---
+++ Max_of_two_integers/Logic1.java
@@ -0,0 +1,20 @@
+package Max_of_two_integers;
+
+import java.util.Scanner;
+
+class Demo{
+    static int max(int a, int b){
+        return (a>b)?a:b;
+    }
+}
+public class Logic1 {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value ");
+        int a = sc.nextInt();
+        System.out.println("Enter b value ");
+        int b = sc.nextInt();
+        sc.close();
+        System.out.println("Max value is  "+Demo.max(a, b));
+        }
+}
