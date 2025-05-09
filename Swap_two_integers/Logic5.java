b33de4f3bbe698983da4712ed1d94c02bab93d4b
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2025-05-09 15:00:34 +0530

    Added new file

---
+++ Swap_two_integers/Logic5.java
@@ -0,0 +1,23 @@
+package Swap_two_integers;
+
+import java.util.Scanner;
+
+class Demo {
+    static void swap(int a, int b) {
+        System.out.println("before swapping a= "+ a +" and b= "+b);
+        a = (a+b)-(b=a);
+        System.out.println("after swapping a = "+a+ " and b = "+b);
+    }
+}
+public class Logic5 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+          System.out.println("Enter a value ");
+          int a = sc.nextInt();
+          System.out.println("Enter b value ");
+          int b = sc.nextInt();
+          sc.close();
+          Demo.swap(a, b);
+    
+}
+}
