e5c6dbfa35c38537b7f97602a55b78eda201486d
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2025-06-05 13:45:00 +0530

    Added new file

---
+++ Arrays/Arrays16.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Program to print 1st smallest, 1st largest, 2nd smallest, 2nd largest and so on
+
+public class Arrays16 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+		System.out.println("Enter array size:");
+		int i,n = obj.nextInt();
+
+		int a[] = new int[n];
+		System.out.println("Enter "+n+" elements...");
+		for(i=0;i<n;i++)
+			a[i] = obj.nextInt();
+
+		Arrays.sort(a);
+
+        int low, high;
+        low = 0;
+        high= n-1;
+        while(low<=high){
+            System.out.print(a[low]+ " "+ a[high]+" ");
+            low++;
+            high--;
+        }
+    }
+}
