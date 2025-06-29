d0da57f128530b85ddc5f7ca8f21de4e281a1940
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2025-06-29 15:27:31 +0530

    Added new file

---
+++ Arrays/Arrays37/Matrix/Matrix4.java
@@ -0,0 +1,40 @@
+package Arrays.Arrays37.Matrix;
+
+import java.util.Scanner;
+
+// 06) Program to read and calcualte sum of all the elements present in the matrix
+
+public class Matrix4 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+		System.out.println("Enter matrix row size:");
+		int rsize = obj.nextInt();
+
+		System.out.println("Enter matrix column size:");
+		int csize = obj.nextInt();
+
+
+		int i,j,sum;
+		int a[][] = new int[rsize][csize];
+
+		System.out.println("Enter matrix element one-by-one:");
+		for(i=0;i<rsize;i++)
+		{
+			for(j=0;j<csize;j++)
+			{
+				a[i][j] = obj.nextInt();
+			}
+		}
+
+		sum=0;
+		for(i=0;i<rsize;i++)
+		{
+			for(j=0;j<csize;j++)
+			{
+				sum=sum+a[i][j];
+			}
+		}
+		System.out.println("Sum ="+sum);
+    }
+}
